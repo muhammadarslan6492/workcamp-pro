324ed3e69b5518ceee89e6f2e6ea7c20d6976d66
muhammad arslan <muhammadarslan6492@gmail.com>
2023-04-29 12:26:34 -0700

    feat: add user model camp model and roles model in data services

---
+++ data.management.services/server/modules/model/roles.js
@@ -0,0 +1,40 @@
+import mongoose from 'mongoose';
+import mongoosePaginate from 'mongoose-paginate-v2';
+
+const rolesSchema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  campId: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: 'Camp',
+  },
+  role: {
+    type: String,
+    values: ['Admin', 'User', 'Hr'],
+    default: 'User',
+  },
+  isVerified: {
+    type: Boolean,
+    default: false,
+  },
+  verifiedBy: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    default: null,
+  },
+  createdBy: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+});
+
+rolesSchema.plugin(mongoosePaginate);
+
+const Roles = mongoose.model('Roles', rolesSchema);
+
+export default Roles;
